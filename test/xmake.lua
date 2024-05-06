5d8a9fd264d30432ab502f7734eb45b0ebfb91cd
kumudiaorong <kmdr.error@gmail.com>
2024-05-06 21:32:41 +0800

    test:style

--- test/xmake.lua
+++ test/xmake.lua
@@ -1,3 +1,4 @@
 add_packages("cli11","fmt")
+add_cxxflags("-fprofile-arcs","-ftest-coverage")
 includes("http","sync","transport")
 
--- xmake.lua
+++ xmake.lua
@@ -2,13 +2,37 @@ set_project("xsl")
 set_xmakever("2.5.1")
 set_version("0.1.0", {build = "%Y%m%d%H%M"})
 
--- set warning all as error
-set_warnings("all", "error")
-set_languages("c++20")
-add_cxxflags("-Wno-error=deprecated-declarations")
+-- add release , debug and coverage modes
+add_rules("mode.debug", "mode.release", "mode.coverage")
 
--- add release and debug mode
-add_rules("mode.debug", "mode.release")
+
+toolchain("gcc")
+    on_load(function (toolchain)
+        if is_mode("debug") then
+            toolchain:set_warnings("all", "error", 'pedantic', 'extra')
+        end
+    end)
+toolchain_end()
+
+toolchain("clang")
+    on_load(function (toolchain)
+        if is_mode("debug") then
+            toolchain:set_warnings("all", "error", 'pedantic', 'extra')
+        end
+    end)
+toolchain_end()
+
+toolchain("msvc")
+    on_load(function (toolchain)
+        if is_mode("debug") then
+            set_warnings("/W4", "/WX")
+            add_defines("_SILENCE_CXX20_DEPRECATED_WARNING")
+        end
+    end)
+toolchain_end()
+
+set_languages("c++20")
+
 
 add_requires("thread-pool", "spdlog", "cli11", "fmt")
 
@@ -16,4 +40,4 @@ add_rules("plugin.compile_commands.autoupdate", {outputdir = "build"})
 
 add_includedirs("$(projectdir)/include")
 
-includes("include", "src", "tests")
+includes("include", "source", "test")

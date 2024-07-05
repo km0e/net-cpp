768151d73f5f3869fc902e342fd0c5a0506e0373
kumudiaorong <kmdr.error@gmail.com>
2024-07-05 17:27:10 +0800

    feat:result add void

--- include/xsl/net/http/router.h
+++ include/xsl/net/http/router.h
@@ -74,7 +74,7 @@ public:
   std::string to_string() const;
 };
 
-using AddRouteResult = Result<std::tuple<>, AddRouteError>;
+using AddRouteResult = Result<void, AddRouteError>;
 
 using RouteResult = Result<IntoSendTasksPtr, RouteError>;
 

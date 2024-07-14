45e8540a1d6dbc4df82cc4b1d368771160db8753
kumudiaorong <kmdr.error@gmail.com>
2024-07-14 14:40:38 +0800

    feat:better mutex lg

--- include/xsl/sync/mutex.h
+++ include/xsl/sync/mutex.h
@@ -3,91 +3,72 @@
 #  define XSL_WHEEL_MUTEX
 #  include "xsl/sync/def.h"
 
+#  include <mutex>
 #  include <shared_mutex>
 XSL_SYNC_NB
 
 template <class T>
-concept Lockable = requires(T t) {
+concept BasicLockable = requires(T t) {
   { t.lock() } -> std::same_as<void>;
   { t.unlock() } -> std::same_as<void>;
+};
+template <BasicLockable T, class V>
+class LockGuard {
+public:
+  LockGuard(T& m, V& v) : lock(m), v(v) {}
+  ~LockGuard() {}
+  decltype(auto) operator->(this auto&& self) { return &self.v; }
+  decltype(auto) operator*(this auto&& self) { return self.v; }
+
+private:
+  std::lock_guard<T> lock;
+  V& v;
+};
+
+template <class T>
+class UnqRes {
+public:
+  UnqRes() : src(), mutex() {}
+  UnqRes(T&& src) : src(std::move(src)), mutex() {}
+  ~UnqRes() {}
+  decltype(auto) lock(this auto&& self) { return LockGuard<std::mutex, T>(self.mutex, self.src); }
+
+private:
+  T src;
+  std::mutex mutex;
+};
+
+template <class T>
+concept Lockable = BasicLockable<T> && requires(T t) {
   { t.try_lock() } -> std::same_as<bool>;
 };
 
 template <class T>
 class ShrdGuard {
 public:
-  ShrdGuard(std::shared_mutex& m, T& t);
-  ~ShrdGuard();
-  T* operator->();
-  T& operator*();
+  ShrdGuard(std::shared_mutex& m, T& t) : m(m), t(t) { m.lock_shared(); }
+  ~ShrdGuard() { m.unlock_shared(); }
+  decltype(auto) operator->(this auto&& self) { return &self.t; }
+  decltype(auto) operator*(this auto&& self) { return self.t; }
 
 private:
   std::shared_mutex& m;
   T& t;
 };
 
-template <class T>
-ShrdGuard<T>::ShrdGuard(std::shared_mutex& m, T& t) : m(m), t(t) {
-  // LOG_TRACE_L3(logger,"");
-  m.lock_shared();
-}
-template <class T>
-ShrdGuard<T>::~ShrdGuard() {
-  // LOG_TRACE_L3(logger,"");
-  m.unlock_shared();
-}
-template <class T>
-T* ShrdGuard<T>::operator->() {
-  return &t;
-}
-template <class T>
-T& ShrdGuard<T>::operator*() {
-  return t;
-}
-
-template <Lockable T, class V>
-class LockGuard {
-public:
-  LockGuard(T& m, V& v);
-  ~LockGuard();
-  V* operator->();
-  V& operator*();
-
-private:
-  T& m;
-  V& v;
-};
-
-template <Lockable T, class V>
-LockGuard<T, V>::LockGuard(T& m, V& v) : m(m), v(v) {
-  // LOG_TRACE_L3(logger,"");
-  m.lock();
-}
-template <Lockable T, class V>
-LockGuard<T, V>::~LockGuard() {
-  // LOG_TRACE_L3(logger,"");
-  m.unlock();
-}
-template <Lockable T, class V>
-V* LockGuard<T, V>::operator->() {
-  return &v;
-}
-template <Lockable T, class V>
-V& LockGuard<T, V>::operator*() {
-  return v;
-}
-template <class C>
+template <class R>
 class ShrdRes {  // rename to SharedResource
 public:
-  ShrdRes() : container(), mutex() {}
+  ShrdRes() : src(), mutex() {}
+  ShrdRes(R&& src) : src(std::move(src)), mutex() {}
   ~ShrdRes() {}
-  ShrdGuard<C> lock_shared() { return ShrdGuard<C>(mutex, container); }
-  LockGuard<std::shared_mutex, C> lock() {
-    return LockGuard<std::shared_mutex, C>(mutex, container);
+  decltype(auto) lock_shared(this auto&& self) { return ShrdGuard<R>(self.mutex, self.src); }
+  decltype(auto) lock(this auto&& self) {
+    return LockGuard<std::shared_mutex, R>(self.mutex, self.src);
   }
 
 private:
-  C container;
+  R src;
   std::shared_mutex mutex;
 };
 XSL_SYNC_NE
